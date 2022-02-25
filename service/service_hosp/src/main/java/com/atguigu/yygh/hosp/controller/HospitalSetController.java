20a705448bd0f23529ebff735c01c31ac0f05a9a
soap-cpp <995560375@qq.com>
2022-02-25 14:20:48 +0800

    第5次提交

--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/HospitalSetController.java
@@ -2,11 +2,14 @@ package com.atguigu.yygh.hosp.controller;
 
 import com.atguigu.yygh.hosp.service.HospitalSetService;
 import com.atguigu.yygh.model.hosp.HospitalSet;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = "医院设置管理")
 @RestController
 @RequestMapping("/admin/hosp/hospitalSet")
 public class HospitalSetController {
@@ -16,6 +19,7 @@ public class HospitalSetController {
     //localhost:8201/admin/hosp/hospitalSet/findAll
 
     //1 查询医院设置表所有信息
+    @ApiOperation(value = "获取所有医院设置")
     @GetMapping("findAll")
     public List<HospitalSet> findAllHospitalSet(){
         List<HospitalSet> list = hospitalSetService.list();
@@ -23,6 +27,7 @@ public class HospitalSetController {
     }
 
     //2 逻辑删除医院设置
+    @ApiOperation(value = "逻辑删除医院设置")
     @DeleteMapping("{id}")
     public boolean removeHospSet(@PathVariable Long id){
         boolean flag = hospitalSetService.removeById(id);
