f3b38cfc6914b3a2adb900a21b077c065ee768b0
soap-cpp <995560375@qq.com>
2022-04-20 10:35:39 +0800

    查看科室和排班

--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/service/DepartmentService.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/service/DepartmentService.java
@@ -2,9 +2,11 @@ package com.atguigu.yygh.hosp.service;
 
 import com.atguigu.yygh.model.hosp.Department;
 import com.atguigu.yygh.vo.hosp.DepartmentQueryVo;
+import com.atguigu.yygh.vo.hosp.DepartmentVo;
 import org.springframework.data.domain.Page;
 
 
+import java.util.List;
 import java.util.Map;
 
 public interface DepartmentService {
@@ -13,4 +15,6 @@ public interface DepartmentService {
     Page<Department> findPageDepartment(int page, int limit, DepartmentQueryVo departmentQueryVo);
 
     void remove1(String hoscode, String depcode);
+    //查询医院科室根据编号
+    List<DepartmentVo> findDeptTree(String hoscode);
 }
